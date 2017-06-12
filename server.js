2594000f0424ba57a22986926d2d1b27cd745db4
Johnnie <johnnie.hard@gmail.com>
2017-06-12 18:01:23 +0200

    initial

---
+++ server.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const buffer = require('@turf/buffer')
+
+app.listen(process.env.PORT || 3000);
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+
+
+app.post('/bufferpoint', function(req, res){
+  console.log(req.body);
+  point = req.body;
+  var buffered = buffer(point, 200, 'meters');
+  res.json(buffered);
+});
